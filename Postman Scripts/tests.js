815f0da8a84a6c30955746c27e8ddc8144432c48
Caio Fortes Perez <caiofperez@gmail.com>
2022-06-08 18:02:39 -0300

    Update postman tests

--- Postman Scripts/tests.js
+++ Postman Scripts/tests.js
@@ -1,5 +1,13 @@
-const schema={}
+const schema={jsonSchema}
 pm.test("Response retorna o contrato esperado",function(){pm.response.to.have.jsonSchema(schema)});
+
+const schema={jsonSchema}
+pm.test("Response retorna o contrato esperado",function(){
+    let nome_da_variavel = pm.response.json();
+    let isSchemaValid = tv4.validate(nome_da_variavel , schema);
+    if (!isSchemaValid) console.log(tv4.error.message);
+    pm.expect(isSchemaValid).to.be.true;
+   });
 
 const token = pm.response.json();
 pm.enviroment.set("token",token.data.token);
